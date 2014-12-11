17b6df78711938edade0ce847b9715be8c84677d
jokercard1986 <jokercard1986@gmail.com>
2014-12-11 21:49:19 +0800

    mix

--- Newidea/src/com/example/newidea/MainActivity.java
+++ Newidea/src/com/example/newidea/MainActivity.java
@@ -2,18 +2,66 @@ package com.example.newidea;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	
+	private EditText editText;
+	private Button button;
+	private CheckBox checkBox;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		editText = (EditText) findViewById(R.id.editText1);
+		button = (Button) findViewById(R.id.button1);
+		checkBox = (CheckBox) findViewById(R.id.checkBox1);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				send();
+			}
+		});
+
+		editText.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (keyCode == KeyEvent.KEYCODE_ENTER
+						&& event.getAction() == KeyEvent.ACTION_DOWN) {
+					send();
+					return true;
+				}
+
+				return false;
+			}
+		});
+	}
+
+	private void send() {
+		String text = editText.getText().toString();
+
+		if (checkBox.isChecked() || text.contains("fuck")) {
+			text = "*******";
+		}
+		
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+		editText.setText("");
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
